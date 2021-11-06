7b5d67ce8378e2d54f18dff07195e78b48fc225d
Italo <italomauss76@outlook.com>
2021-11-05 22:40:15 -0300

    Login

--- App.js
+++ App.js
@@ -4,14 +4,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Tela_Praiano from './scr/components/Tela principal praiano-estabelecimento/Tela_Princ_Praiano';
 import Tela_Estabelecimento from './scr/components/Tela principal praiano-estabelecimento/Tela_Princ_Estabelecimento';
+import Tela_Login from './scr/components/Tela_Login/Login';
 
 const Stack = createNativeStackNavigator();
 
 function MyStack() {
   return (
-    <Stack.Navigator initialRouteName="Tela Praiano">
+    <Stack.Navigator initialRouteName="Login">
       <Stack.Screen name="Tela Praiano" component={Tela_Praiano} />
       <Stack.Screen name="Tela_Estabelecimento" component={Tela_Estabelecimento} />
+      <Stack.Screen name="Login" component={Tela_Login}/>
     </Stack.Navigator>
   );
 }
