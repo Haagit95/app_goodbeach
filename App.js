4f07a83707ca424b843a79a7c9aae9b0e4ab5c31
Leonardo Gabriel Haag <leonardo.g.haag@gmail.com>
2021-11-23 00:36:14 -0300

    subindo esboço telas de menu e cadastro praiano x estab.

--- App.js
+++ App.js
@@ -2,19 +2,33 @@ import * as React from 'react';
 import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-//
+///////////////////////////////////Rotas///////////////////////////////////
 import Tela_Praiano from './scr/components/Tela principal praiano-estabelecimento/Tela_Princ_Praiano';
 import Tela_Estabelecimento from './scr/components/Tela principal praiano-estabelecimento/Tela_Princ_Estabelecimento';
-//
+import Tela_Login from './scr/components/Tela_Login/Login';
+import Cadastro_praiano from './scr/components/telaCadastroPraianoEstabelecimento/Cadastro_praiano';
+import Cadastro_estabelecimento from './scr/components/telaCadastroPraianoEstabelecimento/Cadastro_estabelecimento';
+import Menu_praiano from './scr/components/telaMenu/Menu_praiano';
+import Menu_estabelecimento from './scr/components/telaMenu/Menu_estabelecimento';
+/////////////////////////////////////////////////////////////////////////
 const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
+
 
 function MyStack() {
   return (
-    <Stack.Navigator initialRouteName="">
+    <Stack.Navigator initialRouteName='Login'>
       <Stack.Screen name="Praiano" component={Tela_Praiano} />
-      <Stack.Screen name="Estabelecimento" component={Tela_Estabelecimento} />    
+      <Stack.Screen name="Estabelecimento" component={Tela_Estabelecimento} />  
+      <Stack.Screen name="Cadastro" component={Tela_Estabelecimento} />  
+      <Stack.Screen name="Login" component={Tela_Login} /> 
+      <Stack.Screen name="Cadastro_praiano" component={Cadastro_praiano} />  
+      <Stack.Screen name="Cadastro_estabelecimento" component={Cadastro_estabelecimento} /> 
+      <Stack.Screen name="Login" component={Tela_Login} /> 
+      <Stack.Screen name="Menu_praiano" component={Menu_praiano} /> 
+      <Stack.Screen name="Menu_estabelecimento" component={Menu_estabelecimento} /> 
+ 
+ 
+
     </Stack.Navigator>
   );
 }
