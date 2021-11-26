53f65a29baedd17111f18134e1fafa036f25f188
Leonardo Gabriel Haag <leonardo.g.haag@gmail.com>
2021-11-25 22:01:53 -0300

    erro no projeto

--- App.js
+++ App.js
@@ -4,10 +4,6 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 ///////////////////////////////////Rotas///////////////////////////////////
 import Tela_Estabelecimento from './scr/components/Telas_Estabelecimento/Tela_RelatorioAvaliacao_Estabelecimento';
 import Tela_Login from './scr/components/Tela_Login/Login';
-import Cadastro_praiano from './scr/components/telaCadastroPraianoEstabelecimento/Cadastro_praiano';
-import Cadastro_estabelecimento from './scr/components/telaCadastroPraianoEstabelecimento/Cadastro_estabelecimento';
-import Menu_praiano from './scr/components/telaMenu/Menu_praiano';
-import Menu_estabelecimento from './scr/components/telaMenu/Menu_estabelecimento';
 import Nota_Estab from './scr/components/Telas_Estabelecimento/Nota_Estab';
 import Selo_Estab from './scr/components/Telas_Estabelecimento/Selo';
 import Tela_Avaliacao_Praiano from './scr/components/Telas_Praiano/Tela_Avaliacao_Praiano';
@@ -25,14 +21,10 @@ function MyStack() {
 
       <Stack.Screen name="Login" component={Tela_Login} /> 
 
-      <Stack.Screen name="Cadastro_praiano" component={Cadastro_praiano} /> 
-      <Stack.Screen name="Menu_praiano" component={Menu_praiano} />
       <Stack.Screen name="Avaliação_Praiano" component={Tela_Avaliacao_Praiano} />
       <Stack.Screen name="Ranking_praiano" component={Ranking} /> 
 
       <Stack.Screen name="Estabelecimento" component={Tela_Estabelecimento} />
-      <Stack.Screen name="Cadastro_estabelecimento" component={Cadastro_estabelecimento} /> 
-      <Stack.Screen name="Menu_estabelecimento" component={Menu_estabelecimento} /> 
       <Stack.Screen name="Nota" component={Nota_Estab} />
       <Stack.Screen name="Selo" component={Selo_Estab} />
       <Stack.Screen name="Relatorio_Avaliação" component={Tela_RelatorioAvaliacao_Estabelecimento} />
